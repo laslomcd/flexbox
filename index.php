9c8be58653ee44e4d5a74b37c8bc403546a4d233
Ryan McDonagh <laslomcd@gmail.com>
2018-09-10 14:45:27 -0400

    nav

--- index.php
+++ index.php
@@ -3,13 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <title>FlexBox Tutorial</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="/css/font-awesome.min.css">
     <link rel="stylesheet" href="/build/nav.css">
 </head>
 <body>
 
 <div class="wrapper">
+    <header class="top">
+        <h1><a href="#">What is Flexbox?!</a></h1>
+    </header>
+
+
     <nav class="flex-nav">
+        <a href="#" class=toggleNav>Menu</a>
         <ul>
             <li><a href="#">Item 01</a></li>
             <li><a href="#">Item 02</a></li>
@@ -23,7 +30,26 @@
             <li class="social"><a href="http://instagram.com"><i class="fa fa-instagram"></i></a></li>
         </ul>
     </nav>
+
+    <section class="hero">
+        <img src="https://images.pexels.com/photos/531880/pexels-photo-531880.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
+    </section>
+
+    <section class="details">
+        <p>A simple video course to help you master flexbox</p>
+        <p>Sign Up today and grab all of the videos and exercises</p>
+    </section>
+
+    <section class="signup">
+        <form action="" method="post">
+            <input type="text" placeholder="Your Name">
+            <input type="email" placeholder="Email Address">
+            <input type="submit" value="Sign Me Up!">
+        </form>
+    </section>
+
+    <footer>&copy; Ryan McDonagh</footer>
 </div>
 
 </body>
-</html>
+</html>    
